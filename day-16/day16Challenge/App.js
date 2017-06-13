1db5c810fe5a97cd69c3ad51ffb3b9a2d8ad8fee
Kavya Ravikanti <Kavya@Kavyas-MBP.lan>
2017-06-12 21:36:32 -0400

    fixed nav bar and slide menu, working on routing styling

--- day-16/day16Challenge/App.js
+++ day-16/day16Challenge/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button
+} from "react-native";
 import { NativeRouter, Route, Link } from "react-router-native";
 import SideMenu from "react-native-side-menu";
-import NavBar, { NavButton, NavButtonText, NavTitle } from "react-native-nav";
+// import NavBar, { NavButton, NavButtonText, NavTitle } from "react-native-nav";
 import Settings from "./Settings";
 import Home from "./Home";
 import Menu from "./Menu";
+// import settingpicture from "./settings.png";
 
-const menu = <Menu />;
+const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
+
 export default class App extends Component {
+  state = {
+    isOpen: false
+  };
+
+  updateMenuState = isOpen => {
+    this.setState({ isOpen });
+  };
+
+  onMenuItemSelected = item => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      selectedItem: item
+    });
+  };
   render() {
     return (
-      <SideMenu menu={menu}>
-        <View>
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={isOpen => this.updateMenuState(isOpen)}
+      >
+        <View style={styles.container}>
           <Nav />
-          <Text> hello </Text>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
         </View>
       </SideMenu>
     );
@@ -31,23 +60,17 @@ class Nav extends React.Component {
   render() {
     return (
       <NativeRouter>
-        <View>
-          <NavBar>
-            <NavButton onPress={() => <Link to="/settings" />}>
-              <NavButtonText>
-                {"Settings"}
-              </NavButtonText>
-            </NavButton>
-
-            <NavButton onPress={() => <Link to="/home" />}>
-
-              <Text>
-                {"Home"}
-              </Text>
-
-            </NavButton>
-
-          </NavBar>
+        <View style={styles.nav}>
+          <Link to="/home" underlayColor="#f0f4f7" style={styles.navItem}>
+            <Text> Home </Text>
+          </Link>
+          <Link to="/settings" underlayColor="#f0f4f7" style={styles.navItem}>
+            <View>
+              <Button>
+                {/*<Image source={require(settingpicture)} />*/}
+              </Button>
+            </View>
+          </Link>
           <Route path="/home" component={Home} />
           <Route path="/settings" component={Settings} />
         </View>
@@ -59,14 +82,31 @@ class Nav extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "stretch",
     backgroundColor: "#F5FCFF"
   },
   sidebar: {
     backgroundColor: "green"
   },
+
+  nav: {
+    marginTop: 25,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10
+  },
+
   navbar: {
     backgroundColor: "#F5FCFF"
+  },
+
+  welcome: {
+    textAlign: "center"
   }
 });
